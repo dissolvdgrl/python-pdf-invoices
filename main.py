ba55899322cb5ded9f36a28199ff84e154bcb8a3
dissolvdgrl <c@chilldsgn.com>
2025-07-18 18:52:52 +0200

    optimise variables, add date to PDF

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
-
 def create_pdf(file_path):
     pdf = FPDF(orientation="P", unit="mm", format="A4")
 
     filename = Path(file_path).stem
-    invoice_number = filename.split("-")[0]
-    invoice_date = filename.split("-")[1]
+    invoice_number,date = filename.split("-")
 
     pdf.add_page()
-    pdf.set_font("Courier", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_number}")
+    pdf.set_font("Courier", size=14, style="B")
+
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_number}", ln=True)
+    pdf.cell(w=50, h=8, txt=f"Invoice date: {date}")
+
     pdf.output(f"PDFs/{filename}.pdf")
 
 for filepath in filepaths:
