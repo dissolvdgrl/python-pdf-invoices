9d66ae1ae45cfbaaec430705726054f735a16292
dissolvdgrl <c@chilldsgn.com>
2025-07-18 19:56:23 +0200

    Add total price, logo and company name

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from functools import total_ordering
+
 import pandas as pd
 import glob
 from fpdf import FPDF
@@ -12,6 +14,7 @@ def create_pdf(file_path, data_frame):
     invoice_number,date = filename.split("-")
     header_cells = list(data_frame.columns)
     header_cells = [item.replace("_", " ").title() for item in header_cells]
+    total_price = data_frame['total_price'].sum()
 
     pdf.add_page()
     pdf.set_font("Courier", size=14, style="B")
@@ -27,14 +30,32 @@ def create_pdf(file_path, data_frame):
     pdf.cell(w = 30, h = 8, txt = str(header_cells[3]), border=1)
     pdf.cell(w = 30, h = 8, txt = str(header_cells[4]), border=1, ln=1)
 
+    # Invoice item rows
     for index, row in data_frame.iterrows():
-        pdf.set_font("Courier", size=9, )
+        pdf.set_font("Courier", size=8)
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w = 25, h = 8, txt = str(row["product_id"]), border=1)
         pdf.cell(w = 70, h = 8, txt = str(row["product_name"]), border=1)
         pdf.cell(w = 30, h = 8, txt = str(row["amount_purchased"]), border=1)
         pdf.cell(w = 30, h = 8, txt = str(row["price_per_unit"]), border=1)
         pdf.cell(w = 30, h = 8, txt = str(row["total_price"]), border=1, ln=1)
+
+    # Footer row
+    pdf.set_font("Courier", size=8, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w = 25, h = 8, txt = "", border=1)
+    pdf.cell(w = 70, h = 8, txt = "", border=1)
+    pdf.cell(w = 30, h = 8, txt = "", border=1)
+    pdf.cell(w = 30, h = 8, txt = "", border=1)
+    pdf.cell(w = 30, h = 8, txt = str(total_price), border=1, ln=1)
+
+
+    pdf.set_font("Courier", size=10, style="B")
+    pdf.cell(w = 30, h = 8, txt = f"The total price is R{total_price}", ln=1)
+
+    # Add company name and logo
+    pdf.cell(w = 25, h = 8, txt = "Acme Corp.")
+    pdf.image("ACME_Corporation.png", w = 10)
 
     pdf.output(f"PDFs/{filename}.pdf")
 
